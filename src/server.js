0a80c44c102f622d2abe6bfd8a595f3091ed0cb7
Pradeep Rahangdale <pradeep1999rahangdale@gmail.com>
2026-04-29 17:43:18 +0530

    fix cors

--- src/server.js
+++ src/server.js
@@ -37,6 +37,7 @@ const allowedOrigins = process.env.ALLOWED_ORIGINS
 //     credentials: true,
 //   }),
 // );
+app.use(cors());
 
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
